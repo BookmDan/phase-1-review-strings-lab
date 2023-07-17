ea069c3c93d6fb0b8a83473c8be07ed6fef85bf4
BookmDan <danoh329@gmail.com>
2023-07-17 11:26:52 -0700

    pass

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser[0]
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
